25b9c74c33fa98a91342ec7c7c2fad5faeabc582
carine-ahishakiye <c.yibukabay@alustudent.com>
2024-09-22 16:26:46 +0200

    FarmerProfile.vue

---
+++ frontend/farmer-marketplace/src/components/FarmerProfile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="farmer-profile">
+        <h2>{{ farmer.name }}</h2>
+        <p>{{ farmer.bio }}</p>
+        <h3>Products:</h3>
+        <ul>
+            <li v-for="product in farmer.products" :key="product.id">{{ product.name }} - ${{ product.price }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['farmer'],
+};
+</script>
+
+<style scoped>
+/* Add styles here */
+</style>
